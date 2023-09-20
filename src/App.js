8691f2674fff5d7308e99ea5ef468e1c1e94356e
= <=>
2023-09-20 14:21:37 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import FoodComponent from "./components/FoodComponent";
+import { useEffect, useState } from "react";
+import MenuData from "./data/MenuData";
+import "./App.css";
 function App() {
+  const [foodData, setFoodData] = useState(MenuData); // ข้อมูลทั้งหมด
+  const [dataInPage, setDataInPage] = useState([]); // set จำนวนปุ่ม
+  const [page, setPage] = useState(0); // set index อยู่หน้าไหน
+
+  const pagination = () => {
+    const foodPerPage = 3; //แสดงรายการ 3 รายการ ต่อ 1 หน้า
+    const pages = Math.ceil(MenuData.length / foodPerPage);
+
+    const newFood = Array.from({ length: pages }, (data, index) => {
+      const start = index * foodPerPage; // ได้ช่วงที่ [0,3]
+      return MenuData.slice(start, start + foodPerPage);
+    });
+    return newFood;
+  };
+  const haddlePage = (index) => {
+    setPage(index);
+  };
+
+  useEffect(() => {
+    const paginate = pagination();
+    setDataInPage(paginate);
+    setFoodData(paginate[page]);
+  }, [page]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FoodCard | Pagination</h1>
+      <div className="container">
+        {foodData.map((data, index) => {
+          return <FoodComponent key={index} {...data} />;
+        })}
+      </div>
+      <div className="pagination-container">
+        {dataInPage.map((data, index) => {
+          return (
+            <button
+              key={index}
+              onClick={() => haddlePage(index)}
+              className={`page-btn ${index === page ? "active-btn" : null}`}>
+              {index + 1}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
